c7e64661339627e0d70e1f7e762e5ea90f805d6a
lucasfrotabarroso <lfrota@guarany.com.br>
2024-08-11 20:32:12 -0300

    initial commit

---
+++ src/main/java/com/picpaysimplificado/domain/transaction/Transaction.java
@@ -0,0 +1,37 @@
+package com.picpaysimplificado.domain.transaction;
+
+import com.picpaysimplificado.domain.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name="transactions")
+@Table(name="transactions")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of="id")
+@NoArgsConstructor
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal amount;
+
+    @ManyToOne
+    @JoinColumn(name="sender_id")
+    private User sender;
+
+    @JoinColumn(name="receiver_id")
+    @ManyToOne
+    private User receiver;
+
+    private LocalDateTime timestamp;
+
+
+
+
+
+}
---
+++ src/main/java/com/picpaysimplificado/domain/user/UserType.java
@@ -0,0 +1,11 @@
+package com.picpaysimplificado.domain.user;
+
+
+public enum UserType {
+    COMMON,
+    MERCHANT
+
+
+
+
+}
